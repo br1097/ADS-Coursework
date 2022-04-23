1e6d18fb644cbcb29f8ae0c1e3b08ff5fe90ff95
jameseames <ws18639@bristol.ac.uk>
2022-04-23 20:09:04 +0100

    Multi variable LSMT running now

--- neural network/neuralnetwork.py
+++ neural network/neuralnetwork.py
@@ -5,79 +5,64 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.layers import Dense, LSTM
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import preprocessing
 
-dataframe = pd.read_csv('temperature1.csv', header=0, usecols=["Air temperature"])
+dataframe = pd.read_csv('NNtesting2.csv', header=0)
 dataset = dataframe.values
 dataset = dataset.astype('float')
-#print(dataset)
+#print(dataset[:10])
 # plt.plot(np.arange(0,400), dataset, 'b-')
 # plt.show()
 
 scaler = MinMaxScaler(feature_range=(0,1))
-dataset = scaler.fit_transform(dataset)
+dataset_for_training = scaler.fit_transform(dataset)
 
-look_back = 1
-train_size = int(len(dataset) * 0.7)
-test_size = len(dataset) - train_size
-#train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
-x_train = dataset[0:train_size]
-y_train = dataset[0:train_size]
-x_test = dataset[train_size:len(dataset)]
-y_test = dataset[train_size + 1:len(dataset)]
-x_train = np.reshape(x_train, (x_train.shape[0], look_back, 1))
-y_train = np.reshape(y_train, (y_train.shape[0], look_back, 1))
-x_test = np.reshape(x_test, (x_test.shape[0], 1))
-y_test = np.reshape(y_test, (y_test.shape[0], 1))
+column = list(dataframe)[0]
+air_temp_column = dataframe[column].astype('float')
+#print(columns)
 
-predict_size = 20
-x_test_predict, y_test_predict = x_test[0:int(x_test.shape[0]-predict_size),0], y_test[0:int(y_test.shape[0])-predict_size,0]
-x_predict,y_predict = x_test[-predict_size:x_test.shape[0],0], y_test[-predict_size:y_test.shape[0],0]
-#print(len(x_test_predict)) # = 100
-#print(len(x_predict)) # = 20
+x_train = []
+y_train = []
+
+look_back = 20
+look_forward = 1
+
+for i in range(look_back, len(dataset_for_training) - look_forward +1):
+
+   x_train.append(dataset_for_training[i - look_back:i, 0:dataset.shape[1]])
+   y_train.append(dataset_for_training[i + look_forward - 1:i + look_forward, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
 
 model = Sequential()
 
-model.add(LSTM(500, input_shape=(x_train.shape[1], x_train.shape[2]), activation='sigmoid', return_sequences=True))
-model.add(LSTM(100, activation='sigmoid'))
-
-model.add(Dense(1, activation="sigmoid"))
+model.add(LSTM(64, input_shape=(x_train.shape[1], x_train.shape[2]), activation='sigmoid', return_sequences=True))
+model.add(LSTM(32, activation='sigmoid',return_sequences=False))
+model.add(Dense(y_train.shape[1]))
 
 opt = tf.keras.optimizers.Adam(learning_rate=1e-2)
 model.compile(loss='mean_squared_error', optimizer=opt, metrics=['mean_squared_error'])
-model.fit(x_train, y_train, epochs=100, batch_size=100)
 
-look_forward = 20
-day = 0
-zeros_array = np.zeros((x_test_predict.shape[0]+look_forward,1))
-zeros_array[0:x_test_predict.shape[0],0] = x_test_predict
-while day<=look_forward:
-   x_test_predict = np.reshape(x_test_predict,(x_test_predict.shape[0],look_back,1))
-   pred=model.predict(x_test_predict)
-   x_test_predict = np.reshape(x_test_predict, (x_test_predict.shape[0], 1))
-   zeros_array[0:x_test.shape[0]+day, 0] = pred[-1]
-   x_test_predict_temporary = np.zeros((x_test_predict.shape[0]+1,1))
-   x_test_predict_temporary[0:x_test_predict.shape[0],0] = x_test_predict[:,0]
-   x_test_predict_temporary[-1] = pred[-1]
-   x_test_predict = x_test_predict_temporary
-   day += 1
+model.fit(x_train, y_train, epochs=100, batch_size=100, validation_split=0.1, verbose=1)
 
-final_predict = x_test_predict[-look_forward:len(x_test_predict)]
-final_predict = scaler.inverse_transform(final_predict)
+n_future = 50
+prediction = model.predict(x_train[-n_future:])
+
+prediction_copies = np.repeat(prediction, dataset.shape[1], axis=-1)
+y_prediction_future = scaler.inverse_transform(prediction_copies)[:,0]
+#print(y_prediction_future)
 
 #pd.DataFrame(x_test_predict).to_csv("predicted_data.csv")
-#print(dataset)
-#print(x_test_predict)
 
 fig, ax = plt.subplots()
-ax.plot(np.arange(len(dataset)), dataframe[0:len(dataset)], 'b-') #actual
-ax.plot(np.arange(len(dataset)-len(x_predict),len(dataset)), final_predict, 'r-') #forecast
+#ax.plot(np.arange(len(dataset)), air_temp_column, 'b-') #actual
+ax.plot( np.arange(0,len(dataset)-len(y_prediction_future)), air_temp_column[:-len(y_prediction_future)], 'b-' ) #actual
+ax.plot(np.arange(len(dataset)-len(y_prediction_future),len(dataset)), y_prediction_future, 'r-') #forecast
 #ax.plot(np.arange(len(dataset)-len(x_test_predict),len(dataset)-len(x_predict)),x_test_predict[0:-len(x_predict)], 'c-')
-#ax.set_ylabel('Price (ppl)')
-#plt.xlabel('Time')
-ax.legend(["Real data", "Prediction", "Testing data"], loc="upper left")
+ax.set_ylabel('Air temperature (K)')
+ax.legend(["Real data", "Prediction"], loc="upper left")
 plt.grid()
 plt.show()
