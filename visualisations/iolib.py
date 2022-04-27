d23b180f4fbe52bdc00920ce30a919e19b5de535
Harry Winer <winer.harry@gmail.com>
2022-04-27 13:43:16 +0100

    Did visualisation work, moving to main

--- visualisations/iolib.py
+++ visualisations/iolib.py
@@ -82,9 +82,38 @@ def plot_wind_speed(lng: np.array, lat: np.array, frame: np.array, levels: np.ar
 
     return fig, ax
 
-def plot_time_series(output_filename: str, lng: np.ndarray, lat: np.ndarray, variable: np.ndarray, clouds: np.ndarray=None, centroids=None, size="4p4"):
+def plot_pressure(lng: np.array, lat: np.array, frame: np.array, levels: np.array, image=None):
+    fig = plt.figure()
+    fig.set_size_inches(20, 20)
+    
+    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+    ax.set_extent([lng.min(), lng.max(), lat.min(), lat.max()])
+    
+    if image is None:
+        print("WARNING: Downloading image in plot_wind_speed is innefficient, please specify image in time series")
+        image = cimgt.MapboxTiles(os.environ.get("MAPBOX_TOKEN"), 'satellite-v9')
+    ax.add_image(image, 6)
+
+    fill_cntr = ax.contourf(lng, lat, frame, transform=ccrs.PlateCarree(), cmap="seismic", levels=levels)
+    line_cntr = ax.contour(lng, lat, frame, levels=fill_cntr.levels, colors=['black'])
+    fig.colorbar(fill_cntr, ax=ax, shrink=.7)
+    ax.coastlines()
+    ax.add_feature(cfeature.LAND)
+    ax.add_feature(cfeature.BORDERS)
+        
+    plt.title("Barometric pressure (m/s); Cyclone BoB 1; 1991", size=28)
+    
+    # Add City Tags
+    for city in cities_bangla:
+        plt.plot(city["lng"], city["lat"], "mo", markersize=12)
+        delta = .2
+        ax.annotate(city["name"], xy=(city["lng"] + delta, city["lat"] + delta/2), size=28, color="#D0DF")
+
+    return fig, ax
+
+def plot_time_series(output_filename: str, lng: np.ndarray, lat: np.ndarray, variable: np.ndarray, clouds: np.ndarray=None, centroids=None, size="4p4", kind:str="wind_speed"):
     """
-    output_filename: str # must be .gif
+    output_filename: str # must be .mp4
     lng: np.ndarray # longitude length 810
     lat: np.ndarray # latitude length 790
     variable: np.ndarray # the visualised variable of shape (reference_time, ensemble, lat, lng)\
@@ -96,7 +125,7 @@ def plot_time_series(output_filename: str, lng: np.ndarray, lat: np.ndarray, var
     maxlevel = np.nanmax(variable)
     
     steps = 7
-    levels = [((maxlevel)/steps) * val for val in list(range(steps + 1))]
+    levels = [((maxlevel)/steps) * val for val in list(range(steps + 1))] if maxlevel > 0 and not np.isnan(maxlevel) else None
     
     if clouds is not None:
         maxlevel = np.nanmax(clouds)
@@ -116,8 +145,11 @@ def plot_time_series(output_filename: str, lng: np.ndarray, lat: np.ndarray, var
             error = f"Frame shape incorrect, expected: {(len(lat), len(lng))} got: {frame.shape}"
             raise Exception(error)
         # make the plot, TODO change to kernel function 
-        fig, ax = plot_wind_speed(lng, lat, frame[ensemble], levels, image, clouds[idx][ensemble] if clouds is not None else None, cloud_levels=cloud_levels, size=size)
-        
+        if kind == "wind_speed":
+            fig, ax = plot_wind_speed(lng, lat, frame[ensemble], levels, image, clouds[idx][ensemble] if clouds is not None else None, cloud_levels=cloud_levels, size=size)
+        elif kind == "pressure":
+            fig, ax = plot_pressure(lng, lat, frame[ensemble], levels, image)
+            
         # Add centroids
         if centroids:
             ax.plot(centroids[idx][0], centroids[idx][1], "ro")
@@ -144,3 +176,51 @@ def plot_time_series(output_filename: str, lng: np.ndarray, lat: np.ndarray, var
     # Remove files
     for filename in set(filenames):
         os.remove(filename)
+        
+
+def plot_centroid_path(output_filename: str, lng: np.ndarray, lat: np.ndarray, centroids: np.ndarray):
+    fig = plt.figure()
+    fig.set_size_inches(20, 20)
+    
+    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+    delta = 7
+    ax.set_extent([lng.min()+ delta, lng.max()- delta, lat.min()+ delta, lat.max()- delta])
+    
+    image = cimgt.MapboxTiles(os.environ.get("MAPBOX_TOKEN"), 'satellite-v9')
+    ax.add_image(image, 6)
+    
+    ax.coastlines()
+    ax.add_feature(cfeature.LAND)
+    ax.add_feature(cfeature.BORDERS)
+    
+    plt.title("Path of Cyclone BoB 1, 1991", size=28)
+    
+    # Add City Tags
+    for city in cities_bangla:
+        plt.plot(city["lng"], city["lat"], "mo", markersize=12)
+        delta = .2 # if size == "4p4" else .1
+        ax.annotate(city["name"], xy=(city["lng"] + delta, city["lat"] + delta/2), size=28, color="#D0DF")
+        
+    # add centroids
+    plt.plot(centroids[:,1], centroids[:,0], linestyle="dashed", marker="o", color="#FF0", markersize=12)
+    
+    plt.savefig(f"{OUTPUT_DIRNAME}/{output_filename}")
+    
+
+def plot_blank_map(output_filename: str, lng: np.ndarray, lat: np.ndarray, kind:str):
+    fig = plt.figure()
+    fig.set_size_inches(20, 20)
+    
+    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+    ax.set_extent([lng.min(), lng.max(), lat.min(), lat.max()])
+    
+    image = cimgt.MapboxTiles(os.environ.get("MAPBOX_TOKEN"), 'satellite-v9')
+    ax.add_image(image, 6)
+    
+    ax.coastlines()
+    ax.add_feature(cfeature.LAND)
+    ax.add_feature(cfeature.BORDERS)
+    
+    plt.title(f"{kind}km Resolution", size=28)
+    
+    plt.savefig(f"{OUTPUT_DIRNAME}/{output_filename}")
--- visualisations/vis.py
+++ visualisations/vis.py
@@ -1,29 +1,94 @@
 import datetime
+import sys
 import numpy as np 
 import netCDF4 as nc
 from scipy import stats
-from iolib import plot_time_series
+from iolib import plot_time_series, plot_centroid_path, plot_blank_map
+from memory import get_centroids
 
-PATH = "data/1991TS"
-FILENAME = "fg.T1Hmax.UMRA2T.19910428_19910501.BOB01.1p5km.nc"
+import pandas as pd
 
-fn = f"{PATH}/{FILENAME}"
-fg = nc.Dataset(fn)
 
-lat = fg.variables["latitude"][:]
-lng = fg.variables["longitude"][:]
-wind_speed = fg.variables["wind_speed_of_gust"][:]
-print("Threshold started", datetime.datetime.now().utcnow().__str__())
-thresh_wind = np.array(
-    [[np.where(frame < wind_speed.max() * .3, np.nan, frame) for frame in ensemble ] for ensemble in wind_speed ])
 
-below_wind = np.array(
-    [[np.where(frame < wind_speed.max() * .3, frame, np.nan) for frame in ensemble ] for ensemble in wind_speed ])
 
-clouds = np.array(
-    [[np.where(stats.zscore(frame, nan_policy="omit") > -1, frame, np.nan) for frame in ensemble] for ensemble in below_wind])
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "centroids":
+            PATH = "data/1991TS"
+            FILENAME = "fg.T1Hmax.UMRA2T.19910428_19910501.BOB01.4p4km.nc"
 
-print("Threshold completed", datetime.datetime.now().utcnow().__str__())
+            fn = f"{PATH}/{FILENAME}"
+            fg = nc.Dataset(fn)
 
-plot_time_series('wind_speed1p5.mp4', lng, lat, np.array(thresh_wind), clouds=clouds, size="1p5")
-# plot_time_series("contour.gif", lng, lat, np.array([wind_speed[0]]))
+            lat = fg.variables["latitude"][:]
+            lng = fg.variables["longitude"][:]
+            centroids = get_centroids()
+            
+            filtered = centroids[list(filter(lambda x: x % 5 == 0, list(range(len(centroids)))))]
+            plot_centroid_path('centroids4p4.png', lng, lat, filtered)
+        
+        elif sys.argv[1] == "wind_speed":
+            PATH = "data/1991TS"
+            FILENAME = "fg.T1Hmax.UMRA2T.19910428_19910501.BOB01.4p4km.nc"
+
+            fn = f"{PATH}/{FILENAME}"
+            fg = nc.Dataset(fn)
+
+            lat = fg.variables["latitude"][:]
+            lng = fg.variables["longitude"][:]
+            wind_speed = fg.variables["wind_speed_of_gust"][:]
+            
+            print("Threshold started", datetime.datetime.now().utcnow().__str__())
+            thresh_wind = np.array(
+                [[np.where(frame < wind_speed.max() * .3, np.nan, frame) for frame in ensemble ] for ensemble in wind_speed ])
+
+            below_wind = np.array(
+                [[np.where(frame < wind_speed.max() * .3, frame, np.nan) for frame in ensemble ] for ensemble in wind_speed ])
+
+            clouds = np.array(
+                [[np.where(stats.zscore(frame, nan_policy="omit") > -1, frame, np.nan) for frame in ensemble] for ensemble in below_wind])
+
+            print("Threshold completed", datetime.datetime.now().utcnow().__str__())
+            plot_time_series('wind_speed4p4.mp4', lng, lat, np.array(thresh_wind), clouds=clouds, size="4p4")
+            
+        elif sys.argv[1] == "pressure":
+            
+            PATH = "data/1991TS"
+            FILENAME = "zg.T3Hpoint.UMRA2T.19910428_19910501.BOB01.4p4km.nc"
+
+            fn = f"{PATH}/{FILENAME}"
+            psl = nc.Dataset(fn)
+
+            lat = psl.variables["latitude"][:]
+            lng = psl.variables["longitude"][:]
+            pressure = psl.variables["geopotential_height"][:]
+            
+            plot_time_series("pressure.mp4", lng, lat, pressure, kind="pressure")
+        elif sys.argv[1] == "blank":
+            PATH = "data/1991TS"
+            FILENAME = "zg.T3Hpoint.UMRA2T.19910428_19910501.BOB01.4p4km.nc"
+
+            fn = f"{PATH}/{FILENAME}"
+            psl = nc.Dataset(fn)
+
+            lat4p4 = psl.variables["latitude"][:]
+            lng4p4 = psl.variables["longitude"][:]
+            
+            PATH = "data/1991TS"
+            FILENAME = "zg.T3Hpoint.UMRA2T.19910428_19910501.BOB01.1p5km.nc"
+
+            fn = f"{PATH}/{FILENAME}"
+            psl = nc.Dataset(fn)
+
+            lat1p5 = psl.variables["latitude"][:]
+            lng1p5 = psl.variables["longitude"][:]
+            
+            
+            plot_blank_map("blank4p4.png", lng4p4, lat4p4, "4.4")
+            plot_blank_map("blank1p5.png", lng1p5, lat1p5, "1.5")
+    else:
+        print("Please input argument")
+
+
+
+            
